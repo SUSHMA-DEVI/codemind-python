17751dd3986a881c0d18950169c368893439f9aa
SUSHMA-DEVI <96769402+SUSHMA-DEVI@users.noreply.github.com>
2022-05-19 18:09:54 +0530

    n=int(input()) sum=0 pro=1 temp=n while n>0:     rem=n%10     sum=sum+rem     pro=pro*rem     n=n//10 if sum==pro:     print("Spy Number") else:     print("Not Spy Number")

---
+++ Spy_Number.py
@@ -0,0 +1,13 @@
+n=int(input())
+sum=0
+pro=1
+temp=n
+while n>0:
+    rem=n%10
+    sum=sum+rem
+    pro=pro*rem
+    n=n//10
+if sum==pro:
+    print("Spy Number")
+else:
+    print("Not Spy Number")
